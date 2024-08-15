98cdd1adeea1592bc89c59649c6d266e2f375a72
Omoruyi E. Kelvin <kelvomo9ja@gmail.com>
2024-08-15 19:02:41 +0100

    Update BirdGPT-app.py
    Replace saving downloaded images to hard drive with saving to memory. Improved modularity.

--- BirdGPT-app.py
+++ BirdGPT-app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import requests
-import os
+from io import BytesIO
 from PIL import Image
 import torch
 import torchvision.transforms.v2 as transforms
 
 # Load the pre-trained image recognition model
-model = torch.jit.load(r"BirdGPT scripted.pth", map_location='cpu')
+model = torch.jit.load("BirdGPT scripted.pth", map_location='cpu')
 model.eval()
 
 classes = ['ABBOTTS BABBLER',
@@ -538,49 +538,52 @@ classes = ['ABBOTTS BABBLER',
 st.set_page_config(layout="centered", page_title="Bird Recognition With BirdGPT",menu_items={
     'Report a bug': "https://github.com/mrkomoruyi/BirdGPT/issues/new", 
     'About': "#### This is BirdGPT. An AI bird recognition model. Upload or provide link to an image of a bird and find out it's name! Cool right? :smile:"})
-st.title("Bird Recognition With BirdGPT")
+st.title("Bird Recognition With :blue[BirdGPT]", anchor='title')
+
 # Upload an image
 st.write("### :bird: Upload an image of any bird and find out its name!")
-uploaded_image = st.file_uploader("##### Upload an image", type=["jpg", "png", "jpeg"])
+st.warning('BirdGPT uses AI! Check for mistakes')
 
-uploaded_url = st.text_input("##### Or Enter the URL to the image :link:", placeholder='e.g https://.../golden-eagle.jpg')
+uploaded_image = st.file_uploader("##### Upload an image :camera:", type=["jpg", "png", "jpeg"])
+uploaded_url = None
 
-@st.fragment
+# st.write('###### You can also provide the URL of the image instead.')
+if st.checkbox('Use URL', help= 'Use URL of an image.'):
+    uploaded_url = st.text_input("##### Enter the URL to the image :link:", placeholder='e.g https://.../golden-eagle.jpg')
+
 def get_input():
     if uploaded_url:
         if not uploaded_url.endswith(('.jpg', '.png', '.jpeg')):
-            st.error('Provided link not in accepted format. Link must end in either of: ***.jpg***, .***png*** or ***jpeg***')
+            st.error('Provided link not in acceptable format. Link must end in one of: ***.jpg***, .***png*** or ***jpeg***')
             st.stop()
-        user_img_path = 'user_img.jpg'
-        with open(user_img_path, 'wb') as f:
-            st.success('Downloading Image...')
-            request = requests.get(uploaded_url)
-            f.write(request.content)
-            f.close()
-        image = Image.open(user_img_path).convert("RGB")
+
+        st.success('Fetching image...')
+        request = requests.get(uploaded_url)
+        image = Image.open(BytesIO(request.content)).convert("RGB")
     else:
         image = Image.open(uploaded_image).convert("RGB")
     st.image(image, caption="Uploaded Image", use_column_width=True)
-    #delete the downloaded image gotten from the url
-    if uploaded_url:
-        os.remove(user_img_path)
+
     # Preprocess the image
     transform = transforms.Compose([
         transforms.Resize((224,224)),
         transforms.ToImage(),
-        transforms.ToDtype(torch.float32, scale = True)
-])
+        transforms.ToDtype(torch.float32, scale = True)])
     return transform(image).unsqueeze(0)
 
-if uploaded_image or uploaded_url:
-    input_tensor = get_input()
+def get_prediction(input_tensor):
     # Get model prediction
     with torch.inference_mode():
         output = model(input_tensor).squeeze()
         prediction_prob = output.softmax(0).max().item()
         predicted_class = output.argmax(0).item()
-        st.write(f"###### This is a : ***{classes[predicted_class].title()}***")
-        if prediction_prob >= 0.50:
-            st.write(f"###### ***{prediction_prob*100:.1f}%*** confidence")
-        else:
-            st.write(f"###### ***{prediction_prob*100:.1f}%*** confidence. Please verify as model could be wrong here!")
+    st.write(f"###### This is a/an : ***{classes[predicted_class].title()}***")
+    if prediction_prob >= 0.50:
+        st.write(f"###### ***{int(prediction_prob*100)}%*** confidence")
+    else:
+        st.write(f"###### ***{int(prediction_prob*100)}%*** confidence. Please verify as model could be wrong here!")
+    # return prediction_prob, predicted_class
+
+if uploaded_image or uploaded_url:
+    input_tensor = get_input()
+    get_prediction(input_tensor)
